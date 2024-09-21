c3e1e1feacd9d065b29c63a4c58f861f7649009d
MechanoG <glory_to_the_machine@protonmail.com>
2024-09-20 23:27:27 -0400

    Conexion de los elementos de la interface

--- app/src/main/java/com/example/tesis_01/MainActivity.java
+++ app/src/main/java/com/example/tesis_01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tesis_01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Se declaran el boton de solicitud
+
+    Button gatillo;
+    //TextView salida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Se inicializa el boton y el get text
+        gatillo = findViewById(R.id.button01);
+
+        //Se define la funcion del boton gatillo
+        gatillo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Se realiza el toast
+                Toast.makeText(getApplicationContext(), "Boton presionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
